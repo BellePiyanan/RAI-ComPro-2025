492535e193955044de9e1ba769aaabd94979c36f
BellePiyanan <68011478@kmitl.ac.th>
2025-07-18 05:03:14 +0000

    belle

---
+++ lab-1/ex06.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main(){
+    int number = 69;
+    printf("Decimal: %d\n", number);
+    printf("Octal: %o\n", number);
+    printf("Hexadecimal: %x\n", number);
+
+    return 0;
+
+}
